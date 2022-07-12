117d66f38d489f9d5fbea434bb2cdbc3e3f2bc69
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2022-07-12 17:21:50 -0400

    setting up view, study history

--- src/components/DeckList.js
+++ src/components/DeckList.js
@@ -38,19 +38,42 @@ function DeckList() {
     <div>
       {/* {map decks objects and display out the name and description} */}
       {decks.map((deck) => (
-        <div key={deck.id} className="card">
-          <div className="card-header">{deck.name}</div>
+        <div className="card my-1" key={deck.id}>
           <div className="card-body">
-            <p>{deck.description}</p>
-            <button className="btn btn-primary">View</button>
-            <span> </span>
-            <button className="btn btn-primary">Study</button>
-            <span> </span>
+            <div className="card-title">
+              <h3>{`${deck.name}`}</h3>
+            </div>
+            <div className="card-subtitle">
+              {deck.cards.length === 1 ? (
+                <h6>{`${deck.cards.length} card`}</h6>
+              ) : (
+                <h6>{`${deck.cards.length} cards`}</h6>
+              )}
+            </div>
+            <div className="card-text">{`${deck.description}`}</div>
             <button
-              onClick={() => handleDeckDelete(deck.id)}
-              className="btn btn-danger"
+              type="button"
+              className="btn btn-secondary mx-1 mt-3"
+              onClick={() => history.push(`/decks/${deck.id}`)}
             >
-              Trush
+              <span className="oi oi-eye mr-2"></span>
+              View
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary mx-1 mt-3"
+              onClick={() => history.push(`/decks/${deck.id}/study`)}
+            >
+              <span className="oi oi-book mr-2"></span>
+              Study
+            </button>
+            <button
+              type="button"
+              className="btn btn-danger mx-1 mt-3 float-right"
+              onClick={() => handleDeckDelete(deck.id)}
+            >
+              <span className="oi oi-trash mr-2"></span>
+              Delete
             </button>
           </div>
         </div>
